fb7d00ef998072529120e8df4d6bfe8b3fb57d06
zc <1048300461@qq.com>
2020-04-14 19:41:28 +0800

    实现连接访问 sql server数据库

---
+++ app/src/main/java/com/example/stocksystem/DataBaseUtil.java
@@ -0,0 +1,76 @@
+package com.example.stocksystem;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+
+/**
+ * 连接 sql server 2005
+ * @author zc
+ * @date 2020/4/14
+ */
+public class DataBaseUtil {
+    /**
+     * 获得connection对象
+     * @param ip 主机ip
+     * @param user 用户名
+     * @param pwd 密码
+     * @param db 数据库名
+     * @return connection对象
+     */
+    private static Connection getSQLConnection(String ip, String user, String pwd, String db)
+    {
+        Connection con = null;
+        try
+        {
+            Class.forName("net.sourceforge.jtds.jdbc.Driver");
+              con = DriverManager.getConnection("jdbc:jtds:sqlserver://" + ip + ":1433/" + db + ";useunicode=true;characterEncoding=UTF-8", user, pwd);
+        } catch (ClassNotFoundException e)
+        {
+            e.printStackTrace();
+        } catch (SQLException e)
+        {
+            e.printStackTrace();
+        }
+        return con;
+    }
+
+    /**
+     * 测试连接
+     * @return
+     */
+    public static String testSQL()
+    {
+        String result = "条码区域  -  剩余条码数\n";
+        try
+        {
+            Connection conn = getSQLConnection("192.168.92.145", "sa", "Zc19981221", "gp");
+            String sql = "select name from users";
+            Statement stmt = conn.createStatement();//
+            ResultSet rs = stmt.executeQuery(sql);
+            while (rs.next())
+            {//
+                String s1 = rs.getString("name");
+                result += s1 + "\n";
+                System.out.println(s1);
+            }
+            rs.close();
+            stmt.close();
+            conn.close();
+        } catch (SQLException e)
+        {
+            e.printStackTrace();
+            result += "查询数据异常!" + e.getMessage();
+        }
+        return result;
+    }
+
+    public static void main(String[] args)
+    {
+        testSQL();
+    }
+
+}
+
--- app/src/main/java/com/example/stocksystem/MainActivity.java
+++ app/src/main/java/com/example/stocksystem/MainActivity.java
@@ -2,12 +2,85 @@ package com.example.stocksystem;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.app.Activity;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button btnTest;
+    private Button btnClean;
+    private TextView tvTestResult;
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnTest=findViewById(R.id.btnTestSql);
+        btnClean=findViewById(R.id.btnClean);
+        tvTestResult = findViewById(R.id.tvTestResult);
+        btnTest.setOnClickListener(getClickEvent());
+        btnClean.setOnClickListener(getClickEvent());
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+
+    private View.OnClickListener getClickEvent(){
+        return new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                tvTestResult.setText("...");
+                if(v==btnTest){
+                    test();
+                }
+            }
+        };
+    }
+    private void test()
+    {
+        Runnable run = new Runnable()
+        {
+            @Override
+            public void run()
+            {
+                String ret = DataBaseUtil.testSQL();
+                Message msg = new Message();
+                msg.what=1001;
+                Bundle data = new Bundle();
+                data.putString("result", ret);
+                msg.setData(data);
+                mHandler.sendMessage(msg);
+            }
+        };
+        new Thread(run).start();
+
+    }
+
+    Handler mHandler = new Handler(){
+        public void handleMessage(Message msg) {
+            switch (msg.what)
+            {
+                case 1001:
+                    String str = msg.getData().getString("result");
+                    tvTestResult.setText(str);
+                    break;
+                default:
+                    break;
+            }
+        };
+    };
 }
